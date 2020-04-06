40312a66e003505a320b88bcf6b1f0456ae959ce
LeoAOliveira <leonardo.aoliveira@icloud.com>
2020-04-06 15:37:23 -0300

    Projeto basico de soma de numeros

---
+++ IntentExample/IntentHandler.swift
@@ -0,0 +1,20 @@
+//
+//  IntentHandler.swift
+//  IntentExample
+//
+//  Created by Leonardo Oliveira on 06/04/20.
+//  Copyright © 2020 Leonardo Oliveira. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        // QUer saber quem lida com o Intent detectado
+        return SomaHandler()
+    }
+}
